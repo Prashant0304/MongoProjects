225f611b63b205fa6fe8352087302046059e56cd
unknown <prashantpattanashetti5@gmail.com>
2025-06-28 20:14:24 +0530

    mongo findone object with the help of findone method

--- students/test/reading_test.js
+++ students/test/reading_test.js
@@ -1,18 +1,30 @@
 const assert = require('assert');
 const Student = require('../src/student');
 
-describe("Read the data",()=>{
-    let jason;
+describe("Read the data", () => {
+  let jason;
+    let jason2;
+  beforeEach((done) => { // Fixed: Add done parameter
+    jason = new Student({ name: 'Jason' });
+    jason2 = new Student({name : 'Jason'});
+    jason2.save()
+    jason.save()
+      .then(() => done())
+      .catch((error) => done(error));
 
-    beforeEach(()=>{
-        jason = new Student({
-            name : 'Jason'
-        });
-        jason.save()
-        .then(()=>done())
-    })
-    it("Find the Jason" ,async ()=>{
-      const students = await Student.find({name : 'Jason'})
-      console.log(students)
-    })
-})
+      console.log(jason);
+      console.log(jason2);
+  });
+
+//   it("Find the Jason", async () => {
+//     const students = await Student.find({ name: 'Jason' });
+//     assert(students[0]._id.toString() === jason._id.toString());
+   
+//   });
+
+    it('Find one of the Jasons', async()=>{
+        const students = await Student.findOne({_id: jason._id});
+        console.log(students);
+        assert(students.name == 'Jason')
+    });
+});
