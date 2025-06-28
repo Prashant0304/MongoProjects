b3d3b36a695dbfd73bef764337844738a27a1705
unknown <prashantpattanashetti5@gmail.com>
2025-06-28 18:04:59 +0530

    assert function with new object insertion

--- students/test/create_test.js
+++ students/test/create_test.js
@@ -2,10 +2,14 @@ const assert = require('assert');
 const Student =require("../src/student");
 
 describe("Create the first data ", ()=>{
-    it("Save the student", ()=>{
+    it("Save the student", (done)=>{
        const jason = new Student({
         name : "Jason"
        });
        jason.save()
+       .then(()=>{
+        assert(!jason.isNew)
+        done()
+       })
     })
-})
+});
