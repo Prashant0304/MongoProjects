6de62617356b39d134ea45c34c67a7442ebed085
unknown <prashantpattanashetti5@gmail.com>
2025-06-29 17:33:51 +0530

    mongo db update modifiers

--- students/test/update_test.js
+++ students/test/update_test.js
@@ -3,16 +3,14 @@ const Student = require('../src/student');
 
 describe('Updating records', ()=>{
    let jason;
-   let jason2;
+   
 
    beforeEach((done)=>{
-        jason = new Student({name : 'Jason' , studentNumber :2500})
-        jason2 = new Student({name : 'Jason' , studentNumber :3500 })
-        jason2.save();
+        jason = new Student({name : 'Jason' , studentNumber :2500, articleCount: 5, grade: 10})
         jason.save()
         .then(() => done())
         console.log(jason);
-        console.log(jason2);
+       
     })
 
     // it('set and save', done => {
@@ -33,12 +31,19 @@ describe('Updating records', ()=>{
     //     console.log(res)
     // })
 
-    it('Update Many the Jasons', async()=>{
-        const student = await Student.updateMany({name : 'Jason'}, {studentNumber: 3000})
-        const res = await Student.find({})
-        assert(res[0].studentNumber === 3000)
-        console.log(res)
+    // it('Update Many the Jasons', async()=>{
+    //     const student = await Student.updateMany({name : 'Jason'}, {studentNumber: 3000})
+    //     const res = await Student.find({})
+    //     assert(res[0].studentNumber === 3000)
+    //     console.log(res)
+    // })
+    
+    it('Update grades', async()=>{
+       const artCount = await Student.findOne({name : 'Jason'})
+       const student = await Student.updateOne({name : 'Jason'},{$mul :{grade : artCount.articleCount}})
+       const res = await Student.find({name : 'Jason'})
+       assert(res[0].grade == 50)
+       console.log(res);
     })
-
 
 });
